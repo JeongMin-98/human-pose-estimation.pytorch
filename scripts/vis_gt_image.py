cbd8287ccc7821bb8ae5c2282af5588fe3d880b3
JeongMin-98 <jeongmin981@gmail.com>
2024-06-24 13:46:35 +0900

    [update] vis_gt_image.py

--- scripts/vis_gt_image.py
+++ scripts/vis_gt_image.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 from pycocotools.coco import COCO
-
+from tqdm import tqdm
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='visualize ground truth image (w/o flip,rotate)')
@@ -26,6 +26,11 @@ def arg_parser():
                         required=False,
                         type=str)
 
+    parser.add_argument('--output',
+                        default='output',
+                        help='path/to/images',
+                        type=str)
+
     args = parser.parse_args()
 
     return args
@@ -35,6 +40,10 @@ def vis_gt_image(args):
     # COCO annotation 파일 경로와 이미지 경로
     annotation_file = args.json
     image_folder = args.data
+    output_dir = args.output
+    output_dir = os.path.join(output_dir, 'scripts', image_folder.split('/')[-1])
+
+    os.makedirs(output_dir, exist_ok=True)
     # COCO 객체 생성
     coco = COCO(annotation_file)
 
@@ -42,49 +51,53 @@ def vis_gt_image(args):
     image_ids = coco.getImgIds()
 
     # 배치 사이즈 설정
-    batch_size = 32
+    batch_size = 64
     rows = 8
-    cols = 4
+    cols = 8
+
 
     # 그리드 설정
-    fig, axes = plt.subplots(rows, cols, figsize=(20, 40))
-    axes = axes.flatten()
+    for i in range(0, len(image_ids), batch_size):
+        batch_image_ids = image_ids[i:i + batch_size]
+        fig, axes = plt.subplots(rows, cols, figsize=(20, 30))
+        axes = axes.flatten()
 
-    for i, image_id in enumerate(image_ids[:batch_size]):
-        # load image info
-        image_info = coco.loadImgs(image_id)[0]
-        image_path = os.path.join(image_folder, image_info['file_name'])
+        for j, image_id in tqdm(enumerate(batch_image_ids)):
+            # load image info
+            image_info = coco.loadImgs(image_id)[0]
+            image_path = os.path.join(image_folder, image_info['file_name'])
 
-        # load image
-        image = Image.open(image_path)
+            # load image
+            image = Image.open(image_path).convert('RGB')
 
-        # ax setting
-        ax = axes[i]
-        ax.imshow(image)
-        ax.axis('off')
+            # ax setting
+            ax = axes[j]
+            ax.imshow(image)
+            ax.axis('off')
 
-        # load annotations for image
-        annotation_ids = coco.getAnnIds(imgIds=image_id)
-        annotations = coco.loadAnns(annotation_ids)
+            # load annotations for image
+            annotation_ids = coco.getAnnIds(imgIds=image_id)
+            annotations = coco.loadAnns(annotation_ids)
+            for annotation in annotations:
+                if 'keypoints' in annotation:
+                    keypoints = annotation['keypoints']
+                    for k in range(0, len(keypoints), 3):
+                        keypoint_x = keypoints[k]
+                        keypoint_y = keypoints[k + 1]
+                        keypoint_v = keypoints[k + 2]
+                        if keypoint_v > 0:  # v == 0: not labeled; v == 1: labeled but not visible; v == 2: labeled and visible
+                            ax.plot(keypoint_x, keypoint_y, 'go' if keypoint_v == 2 else 'ro')
+                # # 카테고리 이름 표시
+                # category_id = annotation['category_id']
+                # category_name = coco.loadCats(category_id)[0]['name']
+                # ax.text(x, y - 10, category_name, color='red', fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
+            ax.set_title(image_info['file_name'], fontsize=10)
 
-        for annotation in annotations:
-            bbox = annotation['bbox']
-            # COCO 형식: [x, y, width, height]
-            x, y, width, height = bbox
-            rect = patches.Rectangle((x, y), width, height, linewidth=2, edgecolor='r', facecolor='none')
-            ax.add_patch(rect)
-
-            # 카테고리 이름 표시
-            category_id = annotation['category_id']
-            category_name = coco.loadCats(category_id)[0]['name']
-            ax.text(x, y - 10, category_name, color='red', fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
-
-        # 빈 축 제거
-        for j in range(i + 1, len(axes)):
-            fig.delaxes(axes[j])
-
-    plt.tight_layout()
-    plt.show()
+        # plt.tight_layout()
+        plt.savefig(fname=f'{output_dir}/{i}.png', format='png')
+        print(f"save plot -> {output_dir}/{i}.png")
+        # plt.tight_layout()
+        # plt.show()
 
 
 def main():
