b8cc58098463952534559532e558e291ea70514c
JeongMin-98 <jeongmin981@gmail.com>
2024-07-24 22:25:24 +0900

    [update] Implement inference code for an image.

--- pose_estimation/inference.py
+++ pose_estimation/inference.py
@@ -10,17 +10,20 @@ import _init_paths
 from models.pose_resnet import get_pose_net
 from core.config import config, update_config
 from core.inference import get_max_preds
+from utils.vis import save_debug_images
 
 # Load configuration and model
-cfg_file = 'experiments/coco/resnet50/384x288_d256x3_adam_lr1e-3-RHPE-Foot-N2-Doctor-noflip.yaml'
+cfg_file = 'experiments/coco/resnet50/384x288_d256x3_adam_lr1e-3-RHPE-Foot-N2-Doctor-noflip-copy.yaml'
 update_config(cfg_file)
 
 model = get_pose_net(config, is_train=False)
 model.eval()
+model.init_weights(config.TEST.MODEL_FILE)
 
 # Move model to GPU if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
+
 
 # Image preprocessing
 def preprocess_image(image_path):
@@ -33,18 +36,30 @@ def preprocess_image(image_path):
     image = transform(image)
     return image.unsqueeze(0)  # 배치 차원 추가
 
+
 # Visualize keypoints on the image
 def visualize_keypoints(image_path, keypoints, heatmap_shape):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # print(image.shape)
-    
+
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
     for i, (x, y) in enumerate(keypoints):
         plt.scatter(x / heatmap_shape[0] * 512, y / heatmap_shape[1] * 407, s=100, c='red', marker='x')
         plt.text(x / heatmap_shape[0] * 512, y / heatmap_shape[1] * 407, str(i), fontsize=12, color='yellow')
     plt.show()
+
+
+def get_keypoints_from_heatmap(heatmaps):
+    heatmaps = heatmaps.squeeze().cpu().numpy()
+    keypoints = []
+    for i in range(heatmaps.shape[0]):
+        heatmap = heatmaps[i]
+        y, x = np.unravel_index(np.argmax(heatmap), heatmap.shape)
+        keypoints.append((x, y))
+    return keypoints, (heatmaps.shape[1:])
+
 
 # Main processing function
 def process_image(image_path):
@@ -56,20 +71,11 @@ def process_image(image_path):
     with torch.no_grad():
         output = model(input_image)
 
-    # Get keypoints from heatmaps
-    def get_keypoints_from_heatmap(heatmaps):
-        heatmaps = heatmaps.squeeze().cpu().numpy()
-        keypoints = []
-        for i in range(heatmaps.shape[0]):
-            heatmap = heatmaps[i]
-            y, x = np.unravel_index(np.argmax(heatmap), heatmap.shape)
-            keypoints.append((x, y))
-        return keypoints, (heatmaps.shape[1:])
+    # input_image = input_image.to("cpu")
 
     keypoints, heatmap_shape = get_keypoints_from_heatmap(output)
-
-    # Visualize keypoints on the image
     visualize_keypoints(image_path, keypoints, heatmap_shape)
+
 
 # Image path
 image_path = 'data/coco/Foot/1.2.826.0.1.3680043.8.498.10561448436484761386439086564417613479-c.png'
